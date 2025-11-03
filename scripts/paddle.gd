c7780f791ec377a9ab238fc63bd0a74014458070
o4ted-dev <dtedtarnovski@gmail.com>
2025-11-03 13:10:19 -0500

    ewasf

--- scripts/paddle.gd
+++ scripts/paddle.gd
@@ -3,9 +3,12 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	print("Ready to get started!")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+func do_something():
+	print("Ready to get started!")
